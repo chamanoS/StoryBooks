7f2d0800f07dec7fb0ba6b11cccaec29c14195b1
chamano sidney <chamano.sr@gmail.com>
2020-10-05 08:00:40 +0200

    setting up view engines and middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 //load config
@@ -13,6 +14,9 @@ const app = express()
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
+
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 3000
 
