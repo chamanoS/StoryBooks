f3e19bf646dc1054b98c59bd7cbb2acf2afd8ece
chamano sidney <chamano.sr@gmail.com>
2020-10-05 07:33:38 +0200

    adding the config file

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const dotenv = require('dotenv').
+
+//load config
+dotenv.config({ path: './config/config.env'})
 
 //starting the server
 const app = express()
+
+const PORT = process.env.PORT || 3000
 
 app.listen(5000, console.log('Server has started on port 5000'))
