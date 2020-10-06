8249af370ea270187a678727944a835364fd5845
chamano sidney <chamano.sr@gmail.com>
2020-10-06 11:42:46 +0200

    making a post route for stories

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ connectDB()
 
 const app = express()
 
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
--- routes/stories.js
+++ routes/stories.js
@@ -3,9 +3,23 @@ const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
 const Story = require('../models/story')
 
-
+//show add page
 router.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 })
+
+//process add form
+router.post('/', ensureAuth,async (req, res) => {
+    try{
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch(err) {
+        console.error(err)
+        res.render('error/500')
+    }
+    
+})
+
 
 module.exports = router 
